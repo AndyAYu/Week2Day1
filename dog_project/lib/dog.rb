c4e1854e6c18e8a6bb0afbd46341f81781342238
AndyAYu <andyhkgw@gmail.com>
2021-10-11 13:25:10 -0400

    finish dog.rb

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
 
+    attr_reader :name, :breed, :age, :favorite_foods
+    attr_writer :age
+
+def initialize(name, breed, age, bark, favorite_foods)
+
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+
+end
+
+def bark
+    if @age > 3
+        return @bark.upcase
+    else
+        return @bark.downcase
+    end
+end
+
+def favorite_food?(food)
+@favorite_foods.include?(food.capitalize)
+end
+
 end
